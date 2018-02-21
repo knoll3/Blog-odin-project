fee58465d5279a921689c1aa254506f62b596bc2
knoll3 <noll.kyle@gmail.com>
2018-02-21 20:32:10 +0000

    Create successful subscribe

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -3,4 +3,19 @@ class FollowersController < ApplicationController
   def subscribe
     @follower = Follower.new
   end
+  
+  def create
+    @follower = Follower.new(user_params)
+    if @follower.save
+      # Handle a successful save
+    else
+      render 'subscribe'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:follower).permit(:name, :email)
+    end
 end
