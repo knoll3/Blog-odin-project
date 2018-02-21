408204f7b357d78424e47662935e0bb40ebf95f9
knoll3 <noll.kyle@gmail.com>
2018-02-21 16:53:24 +0000

    Add validity tests

--- test/models/follower_test.rb
+++ test/models/follower_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FollowerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @follower = Follower.new(name: "Example follower", email: "follower@example.com")
+  end
+  
+  test "should be valid" do
+    assert @follower.valid?
+  end
+  
+  test "name should be present" do
+    @follower.name = "    "
+    assert_not @follower.valid?
+  end
+  
+  test "email should be present" do
+    @follower.email = "     "
+    assert_not @follower.valid?
+  end
 end
