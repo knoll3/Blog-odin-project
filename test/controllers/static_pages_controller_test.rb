8b8865b4e7642566fecc66a5179b1585cbffe55d
knoll3 <noll.kyle@gmail.com>
2018-02-21 14:05:51 +0000

    Add titles to static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,35 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | Blog Template"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Blog Template"
   end
 
   test "should get news" do
     get static_pages_news_url
     assert_response :success
+    assert_select "title", "News | Blog Template"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Blog Template"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Blog Template"
   end
 
 end
