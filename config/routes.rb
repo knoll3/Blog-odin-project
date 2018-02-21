7e45995529a229b83615f59867c6ba85a24cf0f0
knoll3 <noll.kyle@gmail.com>
2018-02-21 14:42:59 +0000

    Add working links without testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
-  
   get '/news', to: 'static_pages#news'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
