16e93b817edc8707a1bfd7516ebe381c6e358d15
knoll3 <noll.kyle@gmail.com>
2018-02-21 14:38:16 +0000

    Change router to use path

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,31 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "should get root" do
-    get root_url
-    assert_response :success
-    assert_select "title", "Home | Blog Template"
-  end
-  
-  test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | Blog Template"
   end
 
   test "should get news" do
-    get static_pages_news_url
+    get news_path
     assert_response :success
     assert_select "title", "News | Blog Template"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Blog Template"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Blog Template"
   end
