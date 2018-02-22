65859262c783c804f28c04373de2036691d6ca35
knoll3 <noll.kyle@gmail.com>
2018-02-22 20:15:15 +0000

    Refactor post view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+
+  def correct_date_format(created_at)
+    shifted_time = created_at.in_time_zone("Central Time (US & Canada)")
+    date_format = "%b %d,  %Y at %l:%M %p"
+    shifted_time.strftime(date_format)
+  end
 end
