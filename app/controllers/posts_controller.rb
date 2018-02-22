9e85607f3a3c7fb87b7d197f840137da989f5d75
knoll3 <noll.kyle@gmail.com>
2018-02-22 21:22:25 +0000

    Add create post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Successful post."
+      redirect_to root_path
+    else
+      render 'post'
+    end
   end
 
   def edit
@@ -10,4 +21,10 @@ class PostsController < ApplicationController
 
   def destroy
   end
+  
+  private
+  
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
