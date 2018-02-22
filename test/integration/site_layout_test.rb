29901edd9672821f2b358810d42bad1acfed5d4a
knoll3 <noll.kyle@gmail.com>
2018-02-22 16:32:42 +0000

    Add post validation

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,7 +6,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path
-    assert_select "a[href=?]", about_path, count: 2
+    assert_select "a[href=?]", about_path
     assert_select "a[href=?]", pictures_path
     assert_select "a[href=?]", videos_path
     assert_select "a[href=?]", subscribe_path
