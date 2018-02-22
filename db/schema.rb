57ea09064d8a925a6f8cd5b8acdbb873143b99dc
knoll3 <noll.kyle@gmail.com>
2018-02-22 13:51:55 +0000

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221171508) do
+ActiveRecord::Schema.define(version: 20180222134941) do
 
   create_table "followers", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180221171508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_followers_on_email", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_posts_on_created_at"
   end
 
 end
