92377fa888230519051648f0dbf15dab15c5a67e
knoll3 <noll.kyle@gmail.com>
2018-02-22 18:53:22 +0000

    Add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
   get '/about',     to: 'static_pages#about'
   get '/pictures',  to: 'static_pages#pictures'
   get '/videos',    to: 'static_pages#videos'
   get '/subscribe', to: 'followers#subscribe'
   resources :followers
+  resources :posts
 end
