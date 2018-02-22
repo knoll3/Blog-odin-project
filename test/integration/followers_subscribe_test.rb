ef8b451b11782a3de37f84a3034a7997521f4d79
knoll3 <noll.kyle@gmail.com>
2018-02-22 13:39:02 +0000

    Add successful subscribe test

--- test/integration/followers_subscribe_test.rb
+++ test/integration/followers_subscribe_test.rb
@@ -5,10 +5,21 @@ class FollowersSubscribeTest < ActionDispatch::IntegrationTest
   test "invalid subscibe information" do 
     get subscribe_path
     assert_no_difference 'Follower.count' do
-      post followers_path, params: { follower: { name: "", email: "follower@invalid" } }
+      post followers_path, params: { follower: { name: "", 
+                                                 email: "follower@invalid" } }
     end
     assert_template 'followers/subscribe'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
+  end
+  
+  test "valid subscribe information" do 
+    get subscribe_path
+    assert_difference 'Follower.count', 1 do
+      post followers_path, params: { follower: { name: "Example Follower", 
+                                                email: "follower@example.com" } }
+    end
+    follow_redirect!
+    assert_template 'static_pages/home'
   end
 end
