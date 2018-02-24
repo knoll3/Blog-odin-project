cbd94e605afd11eec9413a4c672121bed8664e5f
Knoll <noll.kyle@gmail.com>
2018-02-23 18:05:44 -0600

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
   root 'static_pages#home'
-  get '/about',     to: 'static_pages#about'
-  get '/pictures',  to: 'static_pages#pictures'
-  get '/videos',    to: 'static_pages#videos'
-  get '/subscribe', to: 'followers#subscribe'
+  get    '/about',     to: 'static_pages#about'
+  get    '/pictures',  to: 'static_pages#pictures'
+  get    '/videos',    to: 'static_pages#videos'
+  get    '/subscribe', to: 'followers#subscribe'
+  get    '/login',     to: 'sessions#new'
+  post   '/login',     to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
   resources :followers
   resources :posts
 end
