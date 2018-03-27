47ba4892e4112da58f5f7fa12368084c200254ab
knoll3 <noll.kyle@gmail.com>
2018-03-27 13:19:31 +0000

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,60 @@
 function newPost() {  
-  var title   = document.createElement("input"); 
-  var content = document.createElement("textarea");
+  var leftColumn = document.getElementById('leftcolumn');
+  var newPostButton = document.getElementById('newPost');
+  leftColumn.removeChild(newPostButton);
+  
+  var form = document.createElement("form");
+    form.setAttribute('class', 'new_post');
+    form.id  = "new_post";
+    form.action = '/posts';
+    form.acceptCharset = "UTF-8";    
+    form.method = 'post';
+    document.getElementById("updatePost").appendChild(form);
+    
+    var authentication = document.createElement('input');
+      authentication.type = "hidden";
+      authentication.name = "authenticity_token";
+      authentication.value = "✓";
+  
+    var titleLabel = document.createElement('label');
+      titleLabel.innerHTML = "Title"
+      document.getElementById("new_post").appendChild(titleLabel);
+    
+    var br = document.createElement("br");
+      document.getElementById("new_post").appendChild(br);
+    
+    var title = document.createElement("input"); 
+      title.type = "text";
+      title.id = "post_title";
+      title.name = "post[title]";
+      title.value = "Title goes here";
+      document.getElementById("new_post").appendChild(title);
+    
+    var br = document.createElement("br");
+      document.getElementById("new_post").appendChild(br);
 
-  title.type = "text";
-  title.id = "newTitle";
-  title.value = "Title goes here";
+    var contentLabel = document.createElement('label');
+      contentLabel.innerHTML = "Content"
+      document.getElementById("new_post").appendChild(contentLabel);
 
-  content.rows = "4";
-  content.cols = "50";
-  content.innerHTML = "This is where the content goes.";
-  content.id = "newContent";
-
-  document.getElementById("newComment").appendChild(title);
-  document.getElementById("newComment").appendChild(content);
+    var br = document.createElement("br");
+      document.getElementById("new_post").appendChild(br);      
+      
+    var content = document.createElement("textarea");
+      content.name = "post[content]";
+      content.id = "post_content";
+      content.rows = "4";
+      content.cols = "50";
+      content.innerHTML = "This is where the content goes.";
+      document.getElementById("new_post").appendChild(content);
+    
+    var br = document.createElement("br");
+      document.getElementById("new_post").appendChild(br);
+  
+    var submit = document.createElement("input");
+      submit.type = "submit";
+      submit.name = "commit";
+      submit.value = "Submit";
+      submit.dataDisableWith = "Submit";
+      document.getElementById("new_post").appendChild(submit);
 }
